50a806016064985ff20132063f3cf053f2114a71
Wilian F. Correa Souza <wilian.silva3@fatec.sp.gov.br>
2024-05-02 16:29:53 -0300

    Update contato.js

--- contato.js
+++ contato.js
@@ -10,6 +10,7 @@ const campoBairro = document.querySelector("#bairro");
 const campoCidade = document.querySelector("#cidade");
 const campoEstado = document.querySelector("#estado");
 const botaoBuscar = document.querySelector("#buscar");
+const mensagemStatus = document.querySelector("#status");
 const mensagem = document.querySelector("#mensagem");
 
 
@@ -19,6 +20,16 @@ botaoBuscar.addEventListener("click", function(event){
     /*'event.preventDefault();' Anula comportamento padrão de recattegar página quando um botão, link ou formulário for acionado*/
     event.preventDefault();
 
-    
+    let cepInformado;
+
+    //para verificar se CEP contém 8 digitos
+    if (campoCep.value.length !== 8) {
+        mensagemStatus.textContent = "Digite um CEP válido"
+        mensagemStatus.style.color = "purple"
+        return;
+    } else{
+        cepInformado = campoCep.value;
+        console.log(cepInformado);
+    }
 })
 
