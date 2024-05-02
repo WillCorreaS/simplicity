8113975d1576c50c1078a5f679d340684da2767a
Wilian F. Correa Souza <wilian.silva3@fatec.sp.gov.br>
2024-05-02 15:20:59 -0300

    Criando script da área CEP

---
+++ contato.js
@@ -0,0 +1,15 @@
+'use strict'
+console.log("olá");
+
+const formulario = document.querySelector("form");
+const campoCep = document.querySelector("#cep");
+const campoEndereco = document.querySelector("#endereco");
+const campoBairro = document.querySelector("#bairro");
+const campoCidade = document.querySelector("#cidade");
+const campoEstado = document.querySelector("#estado");
+const botaoBuscar = document.querySelector("#buscar");
+const mensagem = document.querySelector("#mensagem");
+
+
+
+
